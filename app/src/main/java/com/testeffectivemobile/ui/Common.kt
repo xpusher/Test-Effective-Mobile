d5366597c8810aa4d1eb1c9e94804780561afe06
user <0000>
2024-02-04 00:56:23 +0400

    test EM

--- app/src/main/java/com/testeffectivemobile/ui/Common.kt
+++ app/src/main/java/com/testeffectivemobile/ui/Common.kt
@@ -3,18 +3,34 @@
 package com.testeffectivemobile.ui
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.Divider
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.colorResource
@@ -26,11 +42,13 @@ import androidx.compose.ui.text.input.OffsetMapping
 import androidx.compose.ui.text.input.TransformedText
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.testeffectivemobile.R
 import kotlin.math.absoluteValue
+
 
 val routes = listOf(
     "OnCreateNav",
@@ -181,6 +199,65 @@ fun BottomMenuPreview(
 ) {
     BottomMenu(rememberNavController())
 }
+
+@Composable
+fun DropdownRateCatalog(
+) {
+    var expanded by remember { mutableStateOf(false) }
+    val items = listOf(
+        stringResource(id = R.string.rate_catalog_0),
+        stringResource(id = R.string.rate_catalog_1),
+        stringResource(id = R.string.rate_catalog_3))
+    var selectedIndex by remember { mutableStateOf(0) }
+    Box(modifier = Modifier) {
+
+        Row(                modifier = Modifier
+            .fillMaxWidth()
+            .clickable(onClick = { expanded = true }),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(painter = painterResource(id = R.drawable.ic_rate_catalog),
+                contentDescription = null)
+            Text(
+                items[selectedIndex],
+            )
+
+            Icon(
+                modifier = Modifier.padding(5.dp),
+                painter = painterResource(id = R.drawable.ic_rate_catalog_click),
+                contentDescription = null)
+
+        }
+
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+            modifier = Modifier
+                .fillMaxWidth()
+
+        ) {
+            items.forEachIndexed { index, s ->
+                DropdownMenuItem(onClick = {
+                    selectedIndex = index
+                    expanded = false
+
+                },
+                    text =  {
+                        Text(text = items[index] )
+                    }
+                )
+            }
+        }
+    }
+
+}
+@Preview(showBackground = true)
+@Composable
+fun DropdownRateCatalogPreview(
+) {
+    DropdownRateCatalog()
+}
+
 
 @Composable
 fun UnderConstruction(){
--- app/src/main/java/com/testeffectivemobile/ui/ScreenCatalog.kt
+++ app/src/main/java/com/testeffectivemobile/ui/ScreenCatalog.kt
@@ -4,11 +4,15 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -16,6 +20,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -26,6 +31,7 @@ import com.testeffectivemobile.models.MockyContent
 import com.testeffectivemobile.ui.theme.TestEffectiveMobileTheme
 import kotlinx.coroutines.flow.MutableStateFlow
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ScreenCatalog(
     navHostController: NavHostController,
@@ -58,20 +64,42 @@ fun ScreenCatalog(
     @Composable
     fun ShowCatalog(){
 
-        LazyVerticalGrid(columns = GridCells.Fixed(2)){
-            items(count = mockyContent!!.items.length()) {position->
-                Box(modifier = Modifier
-                    .aspectRatio(1f)
-                    .clickable {
-                        navHostController.navigate(
-                            "ScreenCatalogItem/$position"
-                        )
+        Column {
+            Row(modifier = Modifier.fillMaxWidth()) {
+                Row(modifier = Modifier.weight(1f)) {
+                    DropdownRateCatalog()
+                }
+                Row(modifier = Modifier.weight(1f),
+                    horizontalArrangement = Arrangement.End) {
+                    Icon(painter = painterResource(id = R.drawable.ic_filters_catalog),
+                        contentDescription = null)
 
-                    }) {
-                    Text(text = mockyContent.item(position).description)
+                    Text(
+                        modifier = Modifier,
+                        text = "Фильтр",
+                    )
+
                 }
             }
+            Row {
+                LazyVerticalGrid(columns = GridCells.Fixed(2)){
+                    items(count = mockyContent!!.items.length()) {position->
+                        Box(modifier = Modifier
+                            .aspectRatio(1f)
+                            .clickable {
+                                navHostController.navigate(
+                                    "ScreenCatalogItem/$position"
+                                )
+
+                            }) {
+                            Text(text = mockyContent.item(position).description)
+                        }
+                    }
+                }
+
+            }
         }
+
 
 
     }
